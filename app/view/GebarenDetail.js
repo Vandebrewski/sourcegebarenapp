a5f5074f178ebba376bbb16c8a512c7d943921e5
Robert Dougan <rdougan@me.com>
2014-09-29 13:56:02 +0200

    recreating list video each time it is needed

--- app/view/GebarenDetail.js
+++ app/view/GebarenDetail.js
@@ -34,30 +34,6 @@ Ext.define('Test.view.GebarenDetail', {
             xtype: 'audio',
             name: 'listDetailAudio',
             hidden: true
-        }, 
-        {
-            xtype: 'video',
-            name: 'listDetailVideo',
-//            hidden: true,
-            posterUrl: 'resources/images/bekijkgebaar.png',
-            width: 768,
-            height: 432,            
-            enableControls: false,
-//			preload: false, // default is set to true and during sliding you do not always want video to load. Maybe this help 
-			
-            listeners: {
-           		tap: {
-                   	fn: function () {
-        				if (this.isPlaying())						                    
-    					   this.pause();
-    					                                       					                        
-						else
-				    	   this.play();
-                 	},
-                    element: 'element'  // I moved this outside the function. Is that correct? Works good. Read somwhere it is better. Don't know why.                 	
-                }
-            }
-
         }]
     }
 });
