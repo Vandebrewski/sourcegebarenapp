bfeba3a97af7eb5619e1d1c40c38811be6edc766
Robert Dougan <rdougan@me.com>
2014-10-07 22:14:58 +0200

    revert list view

--- app/view/GebarenDetail.js
+++ app/view/GebarenDetail.js
@@ -8,21 +8,21 @@ Ext.define('Test.view.GebarenDetail', {
         layout: {
             type: 'vbox',
             pack: 'end'
-        	},
+            },
         items: [
             {
                 xtype: 'button',
                 itemId: 'backButton',
-    			cls: 'backButton',
-    			text: 'TERUG'
+                cls: 'backButton',
+                text: 'TERUG'
             },
             {
-            	xtype: 'image',
-            	name: 'listDetailImage',
-	           	cls: 'fade-in',
-            	width: 768,
-            	height: 436
-        	},             
+                xtype: 'image',
+                name: 'listDetailImage',
+                cls: 'fade-in',
+                width: 768,
+                height: 436
+            },             
             {
                 xtype: 'button',
                 itemId: 'listDetailButton', 
@@ -31,8 +31,40 @@ Ext.define('Test.view.GebarenDetail', {
                     var container = this.getParent(),
                     audio = container.down('audio');
                     audio.play();
-        		}
+                }
             },
+            {
+                xclass: 'Test.view.Video',
+                name: 'listDetailVideo',
+                posterUrl: 'resources/images/play-video.png',
+                id: 'listvideo',
+                width: 768,
+                height: 432,           
+                enableControls: false,
+                                
+                listeners: {                    
+                    painted: function () {
+                        this.media.dom.load();
+                    },
+                    tap: {
+                        fn: function () {                                                           
+                            var me = this;
+                            
+                            me.media.dom.addEventListener("playing", function() { // wait for quicktime to be ready so it doesnt show quicktime logo                         
+                                me.play(); 
+                            }, true);
+//                              v.currentTime = 0.2; // don't think I need this
+                            
+                            if (me.isPlaying()) {                                       
+                                me.pause();
+                            } else {                                  
+                                me.play();
+                            }                            
+                        }, // END addEventListener
+                        element: 'element'
+                    } // END tap
+                } // END listeners
+            }, // END video
             {
                 xtype: 'audio',
                 name: 'listDetailAudio',
