81694d07288ea39a454003ce180a6785c31d5454
Robert Dougan <rdougan@me.com>
2014-10-07 18:56:05 +0200

    switch to modal video for carousel

--- app/view/GebarenDetail.js
+++ app/view/GebarenDetail.js
@@ -34,38 +34,6 @@ Ext.define('Test.view.GebarenDetail', {
         		}
             },
             {
-				xclass: 'Test.view.Video',
-				name: 'listDetailVideo',
-                posterUrl: 'resources/images/play-video.png',
-                id: 'listvideo',
-                width: 768,
-                height: 432,           
-                enableControls: false,
-                                
-                listeners: {                	
-                	painted: function () {
-                        this.media.dom.load();
-    				},
-                	tap: {
-                        fn: function () {                        									
-      						var x = document.getElementById("listvideo");
-      						var vid = x.getElementsByTagName("video")[0]; 
-								vid.addEventListener("playing", function() { // wait for quicktime to be ready so it doesnt show quicktime logo							
-									vid.play(); 
-									}, true);
-//								v.currentTime = 0.2; // don't think I need this
-							
-                           	if (this.isPlaying()) {                                       
-                               this.pause();
-                            	} else {                                  
-                               		this.play();
-                            		}                            
-                       		}, // END addEventListener
-                       	element: 'element'
-                   	} // END tap
-               	} // END listeners
-			}, // END video
-            {
                 xtype: 'audio',
                 name: 'listDetailAudio',
                 hidden: true
